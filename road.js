064d4aed4e335bfce46a0b52ad09e60083314efe
Aryan Kumar <lci2020004@iiitl.ac.in>
2023-06-06 00:47:40 +0530

    Added dashed lanes.

--- road.js
+++ road.js
@@ -1,28 +1,30 @@
-class Road{
-    constructor(x,width,laneCount=3){
-        this.x = x; //centre x of the road
-        this.width = width; // width of the road
-        this.laneCount = laneCount;
+class Road {
+  constructor(x, width, laneCount = 3) {
+    this.x = x; //centre x of the road
+    this.width = width; // width of the road
+    this.laneCount = laneCount;
 
-        const infinity = 1000000000;
-        this.top = -infinity;
-        this.bottom = infinity;
-        this.left = x-width/2;
-        this.right = x+width/2;
+    const infinity = 10000000;//setting a much larger value will makes the dashed lanes diappear
+    this.top = -infinity;
+    this.bottom = infinity;
+    this.left = x - width / 2;
+    this.right = x + width / 2;
+  }
+  draw(ctx) {
+    ctx.lineWidth = 5; //width of the road
+    ctx.strokeStyle = "white"; //color of the road
+
+    for (let i = 0; i <= this.laneCount; i++) {
+      const x = lerp(this.left, this.right, i / this.laneCount); //get x position of current lane
+      if (i > 0 && i < this.laneCount)
+        ctx.setLineDash([40, 10]); //draw dashed lines for lanes
+      else ctx.setLineDash([]); //draw solid lines for edges
+      
+  
+      ctx.beginPath();
+      ctx.moveTo(x, this.top);
+      ctx.lineTo(x, this.bottom);
+      ctx.stroke();
     }
-    draw(ctx){
-        ctx.lineWidth = 5;//width of the road
-        ctx.strokeStyle = 'white';//color of the road
-
-        //draw border of road
-        ctx.beginPath();
-        ctx.moveTo(this.left,this.top);
-        ctx.lineTo(this.left,this.bottom);
-        ctx.stroke();
-
-        ctx.beginPath();
-        ctx.moveTo(this.right,this.top);
-        ctx.lineTo(this.right,this.bottom);
-        ctx.stroke();
-    }
+  }
 }
