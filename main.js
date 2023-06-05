8d60d3f202f94fe3c29d69dc367c2b259edfcb31
Aryan Kumar <lci2020004@iiitl.ac.in>
2023-06-06 01:14:28 +0530

    Made the road follow the car, by translating canvas.

--- main.js
+++ main.js
@@ -3,12 +3,16 @@ canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width/2,canvas.width*0.9);
-const car = new Car(road.get_lane_center(3),100,30,50);//x,y,width,height
+const car = new Car(road.get_lane_center(1),100,30,50);//x,y,width,height
 function animate(){
     car.update();
     canvas.height = window.innerHeight;//by resetting height each time, the previous drawn rectangle also gets cleared
+    ctx.save()
+    ctx.translate(0,-car.y+canvas.height*0.7);//translate the canvas so that the road moves but not the car, like a drone camera following the car
     road.draw(ctx);
     car.draw(ctx);
+
+    ctx.restore();
     requestAnimationFrame(animate);
 }
 // setInterval(animate,1000/60);//can use setInterval or requestAnimationFrame to animate
