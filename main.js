d1cbf5b890cd48949ae9a3406935ded3c8c02c03
Aryan Kumar <lci2020004@iiitl.ac.in>
2023-06-06 16:12:47 +0530

    Passed road borders to car.

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width/2,canvas.width*0.9);
 const car = new Car(road.get_lane_center(1),100,30,50);//x,y,width,height
 function animate(){
-    car.update();
+    car.update(road.borders);
     canvas.height = window.innerHeight;//by resetting height each time, the previous drawn rectangle also gets cleared
     ctx.save()
     ctx.translate(0,-car.y+canvas.height*0.7);//translate the canvas so that the road moves but not the car, like a drone camera following the car
