c3bbde241487c116c56f8ce42d8044d7761d419c
Aryan Kumar <lci2020004@iiitl.ac.in>
2023-06-06 20:35:25 +0530

    Add a netwrok canvas for visualize neural net.

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const canvas =  document.getElementById("myCanvas");
-canvas.width = 200;
+const carCanvas =  document.getElementById("carCanvas");
+carCanvas.width = 200;
 
-const ctx = canvas.getContext("2d");
-const road = new Road(canvas.width/2,canvas.width*0.9);
+const ctx = carCanvas.getContext("2d");
+const road = new Road(carCanvas.width/2,carCanvas.width*0.9);
 const car = new Car(road.get_lane_center(1),100,30,50,"AI");//x,y,width,height
 const traffic = [
     new Car(road.get_lane_center(1),30,30,50,"DUMMY",2)
@@ -12,9 +12,9 @@ function animate(){
         traf.update(road.borders,[]);
     });
     car.update(road.borders,traffic);
-    canvas.height = window.innerHeight;//by resetting height each time, the previous drawn rectangle also gets cleared
+    carCanvas.height = window.innerHeight;//by resetting height each time, the previous drawn rectangle also gets cleared
     ctx.save()
-    ctx.translate(0,-car.y+canvas.height*0.7);//translate the canvas so that the road moves but not the car, like a drone camera following the car
+    ctx.translate(0,-car.y+carCanvas.height*0.7);//translate the canvas so that the road moves but not the car, like a drone camera following the car
     road.draw(ctx);
     traffic.forEach(traf => traf.draw(ctx));
     car.draw(ctx);
