1a03fa0537d933afdf4c1aca9465d9ffdd14db8d
Aryan Kumar <lci2020004@iiitl.ac.in>
2023-06-08 00:54:38 +0530

    Added a mask, so that we can assign any color to car.

--- car.js
+++ car.js
@@ -1,5 +1,13 @@
 class Car {
-  constructor(x, y, width, height, type = "DUMMY", maxSpeed = 3) {
+  constructor(
+    x,
+    y,
+    width,
+    height,
+    type = "DUMMY",
+    maxSpeed = 3,
+    color = "blue"
+  ) {
     this.x = x;
     this.y = y;
     this.width = width;
@@ -21,7 +29,20 @@ class Car {
     this.controls = new Controls(type);
 
     this.img = new Image();
-    this.img.src="car.png";
+    this.img.src = "car.png";
+
+    this.mask = document.createElement("canvas");
+    this.mask.width = width;
+    this.mask.height = height;
+    const maskCtx = this.mask.getContext("2d");
+    this.img.onload = () => {
+      maskCtx.fillStyle = color;
+      maskCtx.rect(0, 0, this.width, this.height);
+      maskCtx.fill();
+      maskCtx.globalCompositeOperation = "destination-atop"; //used as intersection between above blue color rectangle and below drawn picture so that we get a blue mask of car
+
+      maskCtx.drawImage(this.img, 0, 0, this.width, this.height);
+    };
   }
 
   update(borders, traffic) {
@@ -136,26 +157,32 @@ class Car {
     this.y -= Math.cos(this.angle) * this.speed;
   }
 
-  draw(ctx,bestCar=false) {
-    // ctx.fillStyle = this.sensor?'blue':'black';
-    // if (this.damaged) ctx.fillStyle = "gray";
-    // ctx.beginPath();
-
-    // ctx.moveTo(this.poygon[0].x, this.poygon[0].y);
-    // for (let i = 1; i < this.poygon.length; i++) {
-    //   ctx.lineTo(this.poygon[i].x, this.poygon[i].y);
-    // }
-    // ctx.fill();
-    ctx.save();
-    ctx.translate(this.x,this.y);
-    ctx.rotate(-this.angle);
-    ctx.drawImage(this.img,
-      -this.width/2,
-      -this.height/2,
-      this.width,
-      this.height);
-    ctx.restore();
+  draw(ctx, bestCar = false) {
     if (this.sensor && bestCar) this.sensor.draw(ctx);
+    //Also shifted sensor rays to top, so that sensor isnt drawn over the car rather under it
+
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(-this.angle);
+    //If car is not damaged, so the colored mask, else show oiginal image
+    if (!this.damaged) {
+      ctx.drawImage(
+        this.mask,
+        -this.width / 2,
+        -this.height / 2,
+        this.width,
+        this.height
+      );
+      ctx.globalCompositeOperation = "multiply";
+    }
+    ctx.drawImage(
+      this.img,
+      -this.width / 2,
+      -this.height / 2,
+      this.width,
+      this.height
+    );
+    ctx.restore();
     // ctx.restore();
   }
 }
