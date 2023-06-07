b79e345c6b53c5a687638f3092b814e086137d23
Aryan Kumar <lci2020004@iiitl.ac.in>
2023-06-07 19:28:12 +0530

    Added save and discard button to save best carr brain.

--- main.js
+++ main.js
@@ -7,9 +7,13 @@ const carCtx = carCanvas.getContext("2d");
 const networkCtx = networkCanvas.getContext("2d");
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 
-const N = 100;
+const N = 1000;
 const cars = generate_cars(N); //x,y,width,height
-const traffic = [new Car(road.get_lane_center(1), 0, 30, 50, "DUMMY", 2)];
+const traffic = [
+  new Car(road.get_lane_center(1), 0, 30, 50, "DUMMY", 2),
+  new Car(road.get_lane_center(0), -200, 30, 50, "DUMMY", 2),
+  new Car(road.get_lane_center(2), -200, 30, 50, "DUMMY", 2),
+];
 
 function generate_cars(N) {
   const cars = [];
@@ -18,17 +22,33 @@ function generate_cars(N) {
   return cars;
 }
 let bestCar = cars[0];
+if (localStorage.getItem("Brain")) {
+  bestCar.brain = JSON.parse(localStorage.getItem("Brain"));
+}
 //The request animation frame also gives the time as parameter
+
+function save() {
+  localStorage.setItem("Brain", JSON.stringify(bestCar.brain));
+}
+console.log(bestCar.brain);
+
+function discard() {
+  localStorage.removeItem("Brain");
+}
+
 function animate(time) {
   traffic.forEach((traf) => {
     traf.update(road.borders, []);
   });
+
   cars.forEach((car) => car.update(road.borders, traffic));
+
   const maxHt = cars.reduce((minn, cur) => Math.min(cur.y, minn), Infinity);
-  //   console.log(maxHt);
   bestCar = cars.find((car) => car.y == maxHt);
+
   carCanvas.height = window.innerHeight; //by resetting height each time, the previous drawn rectangle also gets cleared
   networkCanvas.height = window.innerHeight; //by resetting height each time, the previous drawn rectangle also gets cleared
+
   carCtx.save();
   carCtx.translate(0, -bestCar.y + carCanvas.height * 0.7); //translate the canvas so that the road moves but not the car, like a drone camera following the car
   road.draw(carCtx);
