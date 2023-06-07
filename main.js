00bde752f54cdcb774b89cdd5741ea47d5bbcb89
Aryan Kumar <lci2020004@iiitl.ac.in>
2023-06-07 14:18:38 +0530

    Visualized all levels, and added animation to moving dashes.

--- main.js
+++ main.js
@@ -1,32 +1,33 @@
-const carCanvas =  document.getElementById("carCanvas");
+const carCanvas = document.getElementById("carCanvas");
 carCanvas.width = 200;
-const networkCanvas =  document.getElementById("networkCanvas");
+const networkCanvas = document.getElementById("networkCanvas");
 networkCanvas.width = 300;
 
 const carCtx = carCanvas.getContext("2d");
 const networkCtx = networkCanvas.getContext("2d");
-const road = new Road(carCanvas.width/2,carCanvas.width*0.9);
-const car = new Car(road.get_lane_center(1),100,30,50,"AI");//x,y,width,height
-const traffic = [
-    new Car(road.get_lane_center(1),0,30,50,"DUMMY",2)
-];
-function animate(){
-    traffic.forEach(traf => {
-        traf.update(road.borders,[]);
-    });
-    car.update(road.borders,traffic);
-    carCanvas.height = window.innerHeight;//by resetting height each time, the previous drawn rectangle also gets cleared
-    networkCanvas.height = window.innerHeight;//by resetting height each time, the previous drawn rectangle also gets cleared
-    carCtx.save()
-    carCtx.translate(0,-car.y+carCanvas.height*0.7);//translate the canvas so that the road moves but not the car, like a drone camera following the car
-    road.draw(carCtx);
-    traffic.forEach(traf => traf.draw(carCtx));
-    car.draw(carCtx);
+const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
+const car = new Car(road.get_lane_center(1), 100, 30, 50, "AI"); //x,y,width,height
+const traffic = [new Car(road.get_lane_center(1), 0, 30, 50, "DUMMY", 2)];
 
-    carCtx.restore();
-    Visualizer.drawNetwrok(networkCtx,car.brain);
-    requestAnimationFrame(animate);
+//The request animation frame also gives the time as parameter
+function animate(time) {
+  traffic.forEach((traf) => {
+    traf.update(road.borders, []);
+  });
+  car.update(road.borders, traffic);
+  carCanvas.height = window.innerHeight; //by resetting height each time, the previous drawn rectangle also gets cleared
+  networkCanvas.height = window.innerHeight; //by resetting height each time, the previous drawn rectangle also gets cleared
+  carCtx.save();
+  carCtx.translate(0, -car.y + carCanvas.height * 0.7); //translate the canvas so that the road moves but not the car, like a drone camera following the car
+  road.draw(carCtx);
+  traffic.forEach((traf) => traf.draw(carCtx));
+  car.draw(carCtx);
+
+  carCtx.restore();
+  networkCtx.lineDashOffset = -time/50;
+  Visualizer.drawNetwrok(networkCtx, car.brain);
+  requestAnimationFrame(animate);
 }
 // setInterval(animate,1000/60);//can use setInterval or requestAnimationFrame to animate
- 
+
 animate();
--- visualizer.js
+++ visualizer.js
@@ -1,16 +1,33 @@
 class Visualizer {
   static drawNetwrok(ctx, netwrok) {
     const margin = 50;
-    const top = margin;
+    let top = margin;
     const left = margin;
     const height = ctx.canvas.height - margin * 2;
     const width = ctx.canvas.width - margin * 2;
-    Visualizer.drawLevel(ctx, netwrok.levels[0], left, top, width, height);
+    const numLevels = netwrok.levels.length;
+    const htPerLev = height / numLevels;
+    for (let i = numLevels - 1; i >= 0; i--) {
+      //drawing in reverse, so that biases drawn on intermediate levels dont get over-written by input nodes drawn later on the same
+      ctx.setLineDash([7,3]);//for showing the dashed lines
+      Visualizer.drawLevel(
+        ctx,
+        netwrok.levels[i],
+        left,
+        top,
+        width,
+        htPerLev,
+        i == numLevels - 1 ? ["←", "→", "↑", "↓"] : []
+      );
+      top += htPerLev;
+    }
+    // console.log(htPerLev);
+    // console.log(lerp(0,250,(i+1)/(car.brain.levels.length)))
   }
-  static drawLevel(ctx, level, left, top, width, height) {
+  static drawLevel(ctx, level, left, top, width, height, labels) {
     const { inputs, outputs, weights, biases } = level;
     const radius = 18;
-    
+
     for (let i = 0; i < inputs.length; i++)
       for (let j = 0; j < outputs.length; j++) {
         const x1 = this.#getNodeX(inputs.length, i, left, left + width);
@@ -20,17 +37,36 @@ class Visualizer {
         Visualizer.drawConnection(ctx, x1, y1, x2, y2, getRGBA(weights[i][j]));
       }
 
-
     for (let i = 0; i < inputs.length; i++) {
       const x = this.#getNodeX(inputs.length, i, left, left + width);
-      Visualizer.drawNode(ctx, x, top + height, radius,getRGBA(inputs[i]));
+      Visualizer.drawNode(ctx, x, top + height, radius, "black");
+      Visualizer.drawNode(
+        ctx,
+        x,
+        top + height,
+        radius * 0.6,
+        getRGBA(inputs[i])
+      );
     }
     for (let i = 0; i < outputs.length; i++) {
       const x = lerp(left, left + width, i / (outputs.length - 1));
-      Visualizer.drawNode(ctx, x, top, radius,"black");
-      Visualizer.drawNode(ctx, x, top, radius * 0.6,getRGBA(outputs[i]));
+      Visualizer.drawNode(ctx, x, top, radius, "black");
+      Visualizer.drawNode(ctx, x, top, radius * 0.6, getRGBA(outputs[i]));
+      if (labels.length > 0) {
+        ctx.beginPath();
+        ctx.font = "20px Arial";
+        ctx.textBaseline = "middle";
+        ctx.textAlign = "center";
+        ctx.strokeStyle = "yellow";
+        ctx.lineWidth = 1.5;
+        ctx.strokeText(labels[i], x,top);
 
-      Visualizer.drawNode(ctx, x, top, radius, getRGBA(biases[i]),true);
+        // Draw the arrow inside
+        ctx.fillStyle = "black";
+        ctx.font = "bold 20px Arial";
+        ctx.fillText(labels[i], x, top);
+      }
+      Visualizer.drawNode(ctx, x, top, radius, getRGBA(biases[i]), true);
     }
   }
 
@@ -47,17 +83,17 @@ class Visualizer {
     return lerp(left, right, nodes == 1 ? 0.5 : index / (nodes - 1));
   }
 
-  static drawNode(ctx, x, y, radius, color,dash=null) {
+  static drawNode(ctx, x, y, radius, color, dash = null) {
     ctx.beginPath();
     if (dash) {
       ctx.strokeStyle = color;
       ctx.lineWidth = 2;
       ctx.setLineDash([5, 5]);
-    } else 
-    ctx.fillStyle = color;
+    } else ctx.fillStyle = color;
     ctx.arc(x, y, radius, 0, Math.PI * 2);
     if (dash) ctx.stroke();
     else ctx.fill();
+    ctx.setLineDash([]);
     ctx.closePath();
   }
 }
