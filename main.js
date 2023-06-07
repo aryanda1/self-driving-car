c6590be238f5a9ec49a2c408c1c12e7a22657268
Aryan Kumar <lci2020004@iiitl.ac.in>
2023-06-07 12:55:54 +0530

    Added a neural netwrok visualizer.

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const carCanvas =  document.getElementById("carCanvas");
 carCanvas.width = 200;
+const networkCanvas =  document.getElementById("networkCanvas");
+networkCanvas.width = 300;
 
-const ctx = carCanvas.getContext("2d");
+const carCtx = carCanvas.getContext("2d");
+const networkCtx = networkCanvas.getContext("2d");
 const road = new Road(carCanvas.width/2,carCanvas.width*0.9);
 const car = new Car(road.get_lane_center(1),100,30,50,"AI");//x,y,width,height
 const traffic = [
-    new Car(road.get_lane_center(1),30,30,50,"DUMMY",2)
+    new Car(road.get_lane_center(1),0,30,50,"DUMMY",2)
 ];
 function animate(){
     traffic.forEach(traf => {
@@ -13,13 +16,15 @@ function animate(){
     });
     car.update(road.borders,traffic);
     carCanvas.height = window.innerHeight;//by resetting height each time, the previous drawn rectangle also gets cleared
-    ctx.save()
-    ctx.translate(0,-car.y+carCanvas.height*0.7);//translate the canvas so that the road moves but not the car, like a drone camera following the car
-    road.draw(ctx);
-    traffic.forEach(traf => traf.draw(ctx));
-    car.draw(ctx);
+    networkCanvas.height = window.innerHeight;//by resetting height each time, the previous drawn rectangle also gets cleared
+    carCtx.save()
+    carCtx.translate(0,-car.y+carCanvas.height*0.7);//translate the canvas so that the road moves but not the car, like a drone camera following the car
+    road.draw(carCtx);
+    traffic.forEach(traf => traf.draw(carCtx));
+    car.draw(carCtx);
 
-    ctx.restore();
+    carCtx.restore();
+    Visualizer.drawNetwrok(networkCtx,car.brain);
     requestAnimationFrame(animate);
 }
 // setInterval(animate,1000/60);//can use setInterval or requestAnimationFrame to animate
