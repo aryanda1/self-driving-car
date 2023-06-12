fe6642f0858c15c12ec2372282b572a6dddb7163
Aryan Kumar <lci2020004@iiitl.ac.in>
2023-06-12 18:20:42 +0530

    Added random added traffic with random colors.

--- main.js
+++ main.js
@@ -9,15 +9,17 @@ const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 
 const N = 1;
 const cars = generate_cars(N); //x,y,width,height
-const traffic = [
-  new Car(road.get_lane_center(1), 0, 30, 50, "DUMMY", 2),
-  new Car(road.get_lane_center(0), -200, 30, 50, "DUMMY", 2),
-  new Car(road.get_lane_center(2), -200, 30, 50, "DUMMY", 2),
-  new Car(road.get_lane_center(0), -500, 30, 50, "DUMMY", 2),
-  new Car(road.get_lane_center(1), -500, 30, 50, "DUMMY", 2),
-  new Car(road.get_lane_center(1), -700, 30, 50, "DUMMY", 2),
-  new Car(road.get_lane_center(2), -700, 30, 50, "DUMMY", 2),
+let traffic = [
+  new Car(road.get_lane_center(1), 0, 30, 50, "DUMMY", 2, getRandomColor()),
+  new Car(road.get_lane_center(0), -200, 30, 50, "DUMMY", 2, getRandomColor()),
+  new Car(road.get_lane_center(2), -200, 30, 50, "DUMMY", 2, getRandomColor()),
+  new Car(road.get_lane_center(0), -500, 30, 50, "DUMMY", 2, getRandomColor()),
+  new Car(road.get_lane_center(1), -500, 30, 50, "DUMMY", 2, getRandomColor()),
+  new Car(road.get_lane_center(1), -700, 30, 50, "DUMMY", 2, getRandomColor()),
+  new Car(road.get_lane_center(2), -700, 30, 50, "DUMMY", 2, getRandomColor()),
 ];
+
+const max_traffic_Cars = 7;
 
 function generate_cars(N) {
   const cars = [];
@@ -30,7 +32,7 @@ if (localStorage.getItem("Brain")) {
   cars.forEach((car,i)=> {
     car.brain = JSON.parse(localStorage.getItem("Brain"));
     if(i!=0)
-    NeuralNetwrok.mutate(car.brain,0.1);
+    NeuralNetwrok.mutate(car.brain,0.15);
   });
 }
 //The request animation frame also gives the time as parameter
@@ -45,8 +47,30 @@ function discard() {
 }
 
 function animate(time) {
+  
+  traffic = traffic.filter((traf) => traf.y < bestCar.y+400);
+  while (traffic.length < max_traffic_Cars) {
+    let i = 200;
+    // Generate new traffic car
+    const laneIndex = Math.floor(Math.random() * road.laneCount); // Random lane index
+    const laneCenter = road.get_lane_center(laneIndex);
+    const mainCar = bestCar; // Assuming the main car is at index 0
+    const newTrafficCar = new Car(
+      laneCenter,
+      mainCar.y - carCanvas.height -i, // Start above the main car, adjust the value as needed
+      30,
+      50,
+      "DUMMY",
+      2,
+      getRandomColor()
+    );
+    // console.log(newTrafficCar)
+    traffic.push(newTrafficCar);
+    i+=400;
+  }
   traffic.forEach((traf) => {
     traf.update(road.borders, []);
+    // console.log(traf.y);
   });
 
   cars.forEach((car) => car.update(road.borders, traffic));
